6de66a358a73038bbdeda820c61b19005b6ea5b7
Daniel <daniel_ansari@sennalabs.com>
2021-05-13 03:38:01 +0700

    remove breakpoint

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def doorkeeper
-    byebug
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
