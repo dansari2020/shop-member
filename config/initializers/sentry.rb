56bb9b8fd8df055c7c33cb76264856d4c4f34406
Daniel <daniel_ansari@sennalabs.com>
2021-08-18 17:25:06 +0700

    fix: sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 'https://556bc20e1aa0414a993cb5cfd5a101b6@o659034.ingest.sentry.io/5763869'
+  config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
