412f6a70259197aff231400de9ea6f1bda8344d5
Daniel <daniel_ansari@sennalabs.com>
2021-08-18 19:37:41 +0700

    fix: show message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,17 @@
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
-
+$(document).on("turbolinks: load", function () {
+  console.log("turbo loaded");
+});
 $(function () {
+  console.log("loaded");
   $(".data-explore").click(function () {
+    console.log("clicked");
     var link = "/explore/" + $("#api").val();
+    $("#display-json").text(
+      "Please Wait - Fetching the /api/v1/" + $("#api").val() + " API ..."
+    );
     $("#display-json").load(link, function (text, textStatus, req) {
       if (textStatus === "error") {
         $(this).text("Error: Please enter a valid API address");
